3a0f004b98973c37b63315a0db566712e8a79001
saulandresn <viripms18@gmail.com>
2026-09-20 16:44:52 -0500

    chore: add app/db.py

---
+++ app/db.py
@@ -0,0 +1,11 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./traffic.db")
+connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
+engine = create_engine(DATABASE_URL, pool_pre_ping=True, connect_args=connect_args)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+class Base(DeclarativeBase):
+    pass
